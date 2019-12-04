c08d624194dbb301d410e78da0dcb38c6c310c6c
AK20399 <aakash.darji20@gmail.com>
2019-12-04 23:57:21 +0530

    added delete operation

--- delete/delete.php
+++ delete/delete.php
@@ -8,5 +8,36 @@
 </head>
 <body>
    <h1>Delete</h1> 
+   <form>
+   <input type="text" name="id" placeholder="Enter Id number"><br><br>
+   <input type="submit" value="submit" name="submit"><br><br>
+   </form>
+   <?php 
+   include('.././phpstuff/connection.php');
+    if(isset($_REQUEST['submit'])){
+        $id = $_REQUEST['id'];
+        $sql = "DELETE FROM data WHERE id =$id";
+        if($conn->query($sql)===true){
+            echo 'Record with id ' ,$id ,' deleted Successfully';
+        }
+        else{
+            echo 'Error Deleting Data';
+        }
+    }
+   ?>
+   <input type="submit" value="Delete all data" name="deleteall" onclick="deleteall()">
+   <?php 
+if($_GET){
+        if(isset($_GET['deleteall'])){
+            deleteall();
+        }
+    }
+
+    function deleteall()
+    {
+       echo "The select function is called.";
+    }
+   ?>
+   
 </body>
 </html>
